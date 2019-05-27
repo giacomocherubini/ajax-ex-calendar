a8a07de12086a46e459f9dd71b74e79983601a42
giacomocherubini <giacomo.cherubini1@gmail.com>
2019-05-27 15:37:00 +0200

    fine

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-$(document).ready(function()  {
+$(document).ready(function() {
 
   var source = $("#entry-template").html();
   var template_function = Handlebars.compile(source);
@@ -9,36 +9,37 @@ $(document).ready(function()  {
   var min_date = '2018-01-01';
   var max_date = '2018-12-01';
 
-   disegna_mese(moment_date);
-   disegna_festivita(moment_date.month());
+  disegna_mese(moment_date);
+  disegna_festivita(moment_date.month());
 
 
   function disegna_mese(current_date) {
     // resetto il contenitore del calendario
     $('.day').html('');
-  // leggo quanti giorni ci sono nel mese corrente
-  var giorni = moment_date.daysInMonth();
+    // leggo quanti giorni ci sono nel mese corrente
+    var giorni = moment_date.daysInMonth();
 
-  var mese = moment_date.format('MMMM');
-  var month = moment_date.month();
-  $('.month').text(mese + ' ' + '2018');
+    var mese = moment_date.format('MMMM');
+    var month = moment_date.month();
+    $('.month').text(mese + ' ' + '2018');
 
+    for (var i = 0; i < giorni; i++) {
 
-  for (var i = 0; i < giorni; i++) {
+      var new_data_test = '2018-'+moment_date.format('MM')+'-'+parseInt(i+1);
+      // => 2018-01-1 // 2018-01-01
+      new_data_test = moment(new_data_test).format('YYYY-MM-DD'); //=> 2018-01-01
 
-    var giorno = {
-      'giorni_calendario' : parseInt(i+1)
+      var giorno = {
+        'giorni_calendario' : parseInt(i+1),
+        'giorno_iso': new_data_test
+      };
+      var html = template_function(giorno);
+      $('.day').append(html);
     };
-    var html = template_function(giorno);
-    $('.day').append(html);
-  };
 
-
-
-}
+  }
 
   $('.avanti').click(function() {
-    console.log(moment_date.format('MMMM'));
     if (moment_date.isSameOrAfter(max_date)) {
       alert('mese non possibile');
       $(this).attr('disabled', true);
@@ -52,7 +53,6 @@ $(document).ready(function()  {
   });
 
   $('.indietro').click(function() {
-    console.log(moment_date.format('MMMM'));
     if (moment_date.isSameOrBefore(min_date)) {
       alert('mese non possibile');
       $(this).attr('disabled', true);
@@ -66,35 +66,26 @@ $(document).ready(function()  {
   });
 
   function disegna_festivita(mese) {
-      $.ajax({
-        'url': 'https://flynn.boolean.careers/exercises/api/holidays',
-        'data': {
-          'year': '2018',
-          'month': mese
-        },
-        'method': 'GET',
-        'success': function(data) {
-
-          var festivita = data.response;
-          // console.log(festivita);
-          // ciclo le festivita restituite dall'api
-          for (var i = 0; i < festivita.length; i++ ) {
-            // festivita corrente
+    $.ajax({
+      'url': 'https://flynn.boolean.careers/exercises/api/holidays',
+      'data': {
+        'year': '2018',
+        'month': mese
+      },
+      'method': 'GET',
+      'success': function(data) {
+        var festivita = data.response;
+        // ciclo le festivita restituite dall'api
+        for (var i = 0; i < festivita.length; i++ ) {
+          // festivita corrente
           var festa = festivita[i];
-          // console.log(festa);
-          // recupero  l'item con la data corrispondente alla festivita corrente
-          var giorno_festa = $('.day.numero_giorni[data-giorno_iso="'+festa.date+'"]');
-          console.log(giorno_festa);
-          // aggiungo la classe che fa il testo rosso
-          giorno_festa.addClass('festivita');
-          // concateno il nome della festivita
-          giorno_festa.append(' - ' + festa.name);
+          // recupero  l'item con la data corrispondente alla festivita corrente - aggiungo la classe che fa il testo rosso - concateno il nome della festivita
+          $('.day .numero_giorni[data-giorno_iso="'+festa.date+'"]').addClass('festivita').append(' - ' + festa.name);
         }
-
       },
       'error': function() {
         alert('si e verificato un errore');
       }
     });
-    }
+  }
 });
